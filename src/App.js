4cd266be2518e871073eabe984c98803b9692f38
Rohit <1rohitsangha@gmail.com>
2024-05-14 22:17:55 -0600

    test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import './App.css';
-import topImage from './topImage.jpg';
+import topImage from './topimage.png';
 import bottomImage from './bottomImage.jpg';
 
 function useQuery() {
